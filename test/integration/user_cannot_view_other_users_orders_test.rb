614c969fb5e1b7a4047234423d0e6121035b4afa
Steven Olson <steveoscaro@gmail.com>
2016-01-16 14:22:00 -0700

    add integration test to ensure a user cannot view another users order info

---
+++ test/integration/user_cannot_view_other_users_orders_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class UserCannotViewOtherUsersOrdersTest < ActionDispatch::IntegrationTest
+  test "the truth" do
+    user1, user2 = create_list(:user, 2)
+    order1 = user1.orders.create
+    order2 = user1.orders.create
+
+    ApplicationController.any_instance.stubs(:current_user).returns(user2)
+
+    assert_raises(ActiveRecord::RecordNotFound) do |variable|
+      visit order_path(order1)
+    end
+  end
+end
