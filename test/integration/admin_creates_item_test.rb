b7d822306ec234ab347f759d7f96271dd674eda0
Obtuse <theobtuseautodidact@gmail.com>
2016-01-17 15:40:12 -0700

    add admin item create happy path complete with test passing
    need sad paths and validations

--- test/integration/admin_creates_item_test.rb
+++ test/integration/admin_creates_item_test.rb
@@ -3,6 +3,10 @@ require "test_helper"
 class AdminCreatesItemTest < ActionDispatch::IntegrationTest
   test "admin can create a stache" do
     admin = create(:admin)
+    category = Category.create(title: "cowboy")
+    Category.create(title: "hipster")
+    Category.create(title: "pirate")
+    category2 = Category.create(title: "flamboyent")
     ApplicationController.any_instance.stubs(:current_user).returns(admin)
 
     visit admin_dashboard_path
@@ -14,6 +18,8 @@ class AdminCreatesItemTest < ActionDispatch::IntegrationTest
     fill_in "Description", with: "impressive new stache"
     fill_in "Price", with: 12
     fill_in "Image Url", with: "http://i.imgur.com/kMx8J0g.png "
+    find(:css, "#stache_categories_#{category.id}[value='#{category.id}']").set(true)
+    find(:css, "#stache_categories_#{category2.id}[value='#{category2.id}']").set(true)
 
     click_on "Create New Stache"
     stache = Stache.last
@@ -21,5 +27,7 @@ class AdminCreatesItemTest < ActionDispatch::IntegrationTest
     assert page.has_content?("test_stache")
     assert page.has_content?("impressive new stache")
     assert page.has_css?("img[src*='http://i.imgur.com/kMx8J0g.png']")
+    assert page.has_content?(category.title)
+    assert page.has_content?(category2.title)
   end
 end
