f0310832678d9308b986a9d4f992f4917ffbd60b
Steven Olson <steveoscaro@gmail.com>
2016-01-16 18:39:01 -0700

    updated logo and index cards for products

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,7 @@ class ApplicationController < ActionController::Base
   end
 
   def random_stache
-    num = rand(1..3)
+    num = rand(1..4)
     "../assets/logos/#{num}.png"
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-pic = "http://i.imgur.com/v1b4ixD.png"
+pic = "http://i.imgur.com/kMx8J0g.png"
 stache_1 = Stache.create(name: "Handlebar", description: "things", price: 5.0,
                          image_url: pic)
 stache_2 = Stache.create(name: "Chevron", description: "things", price: 3.0,
