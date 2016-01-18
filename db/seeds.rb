3f6f993b8aded27b988b60b2c11e322f70ad57f6
Steven Olson <steveoscaro@gmail.com>
2016-01-18 11:44:53 -0700

    styled the stache show page with a simulation of the video box

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 description = "Raw denim paleo brunch, +1 cold-pressed fanny pack
 intelligentsia cred. Thundercats small batch gastropub, sartorial hella you
 probably haven't heard of them occupy before they sold out migas bicycle
 rights tacos"
+
 pic = "http://i.imgur.com/kMx8J0g.png"
 
 cat_1 = Category.create(title: "Old Fashioned")
@@ -17,79 +11,66 @@ cat_3 = Category.create(title: "Pirate")
 cat_4 = Category.create(title: "Flamboyant")
 
 # some variable assigments kept for order creation down below
-stache_1 = cat_1.staches.create(name: "Fluffer", description: description,
+stache_1 = cat_1.staches.create(
+                         name: "Fluffer", description: description,
                          price: 5.0,
                          image_url: "http://i.imgur.com/4NHyiF6.png",
                          stache_url: "http://i.imgur.com/HWGUgox.png")
-stache_2 = cat_1.staches.create(name: "Chevron", description: description, price: 3.0,
+stache_2 = cat_1.staches.create(
+                         name: "Chevron", description: description,
+                         price: 3.0,
                          image_url: "http://i.imgur.com/16uh0nF.png",
                          stache_url: "http://i.imgur.com/Qjwi1YV.png")
 cat_1.staches.create(name: "Trapper", description: description, price: 10.0,
-                         image_url: "http://i.imgur.com/3J3agKC.png",
-                         stache_url: "http://i.imgur.com/rJ71NVK.png")
+                     image_url: "http://i.imgur.com/3J3agKC.png",
+                     stache_url: "http://i.imgur.com/rJ71NVK.png")
 cat_1.staches.create(name: "Roosevelt", description: description, price: 9.0,
-                         image_url: "http://i.imgur.com/Ia027l3.png",
-                         stache_url: "http://i.imgur.com/IfWnBMd.png")
-stache_5 = cat_2.staches.create(name: "Denim Soul", description: description,
+                     image_url: "http://i.imgur.com/Ia027l3.png",
+                     stache_url: "http://i.imgur.com/IfWnBMd.png")
+stache_5 = cat_2.staches.create(
+                         name: "Denim Soul", description: description,
                          price: 4.0,
                          image_url: "http://i.imgur.com/29KrsNC.png",
                          stache_url: "http://i.imgur.com/xvAxx8E.png")
 cat_2.staches.create(name: "Gastropub", description: description,
-                         price: 4.0,
-                         image_url: "http://i.imgur.com/Rdrd1Ph.png",
-                         stache_url: "http://i.imgur.com/NCMIMQy.png")
+                     price: 4.0,
+                     image_url: "http://i.imgur.com/Rdrd1Ph.png",
+                     stache_url: "http://i.imgur.com/NCMIMQy.png")
 cat_2.staches.create(name: "Too Cool", description: description,
-                         price: 4.0,
-                         image_url: "http://i.imgur.com/sWH2YXv.png",
-                         stache_url: "http://i.imgur.com/MQEXa4Y.png")
+                     price: 4.0,
+                     image_url: "http://i.imgur.com/sWH2YXv.png",
+                     stache_url: "http://i.imgur.com/MQEXa4Y.png")
 cat_2.staches.create(name: "I'm Bored", description: description,
-                         price: 4.0,
-                         image_url: "http://i.imgur.com/UaWJ4ub.png",
-                         stache_url: "http://i.imgur.com/PLHQykF.png")
+                     price: 4.0,
+                     image_url: "http://i.imgur.com/UaWJ4ub.png",
+                     stache_url: "http://i.imgur.com/PLHQykF.png")
 cat_3.staches.create(name: "Arrrr", description: description, price: 4.0,
-                         image_url: "http://i.imgur.com/vctVE2S.png",
-                         stache_url: "http://i.imgur.com/ts6Cs7W.png")
+                     image_url: "http://i.imgur.com/vctVE2S.png",
+                     stache_url: "http://i.imgur.com/ts6Cs7W.png")
 cat_3.staches.create(name: "Matey", description: description, price: 4.0,
-                          image_url: "http://i.imgur.com/sqpx1Ah.png",
-                          stache_url: "http://i.imgur.com/zHitnkQ.png")
+                     image_url: "http://i.imgur.com/sqpx1Ah.png",
+                     stache_url: "http://i.imgur.com/zHitnkQ.png")
 cat_3.staches.create(name: "'Ol Wood Leg", description: description,
-                          price: 4.0,
-                          image_url: "http://i.imgur.com/9bD5Rvo.png",
-                          stache_url: "http://i.imgur.com/BDXPjkM.png")
+                     price: 4.0,
+                     image_url: "http://i.imgur.com/9bD5Rvo.png",
+                     stache_url: "http://i.imgur.com/BDXPjkM.png")
 cat_3.staches.create(name: "Shiver Me Timbers", description: description,
-                          price: 4.0,
-                          image_url: "http://i.imgur.com/RShmizO.png",
-                          stache_url: "http://i.imgur.com/yExd7hd.png")
+                     price: 4.0,
+                     image_url: "http://i.imgur.com/RShmizO.png",
+                     stache_url: "http://i.imgur.com/yExd7hd.png")
 cat_4.staches.create(name: "Fabulous", description: description,
-                          price: 4.0,
-                          image_url: "http://i.imgur.com/oRpl6IL.png",
-                          stache_url: "http://i.imgur.com/SKWzHIb.png")
+                     price: 4.0,
+                     image_url: "http://i.imgur.com/oRpl6IL.png",
+                     stache_url: "http://i.imgur.com/SKWzHIb.png")
 cat_4.staches.create(name: "Diva", description: description, price: 4.0,
-                          image_url: "http://i.imgur.com/eV1i2oy.png",
-                          stache_url: "http://i.imgur.com/f0EAa6p.png")
+                     image_url: "http://i.imgur.com/eV1i2oy.png",
+                     stache_url: "http://i.imgur.com/f0EAa6p.png")
 cat_4.staches.create(name: "Kitten", description: description, price: 4.0,
-                          image_url: "http://i.imgur.com/LM4paeF.png",
-                          stache_url: "http://i.imgur.com/cx1omcB.png")
+                     image_url: "http://i.imgur.com/LM4paeF.png",
+                     stache_url: "http://i.imgur.com/cx1omcB.png")
 cat_4.staches.create(name: "Yay!", description: description, price: 4.0,
-                          image_url: "http://i.imgur.com/XtsVPx6.png",
-                          stache_url: "http://i.imgur.com/UfIug97.png")
-
-# stache_1.categories << cat_1
-# stache_2.categories << cat_1
-# stache_3.categories << cat_1
-# stache_4.categories << cat_1
-# stache_5.categories << cat_2
-# stache_6.categories << cat_2
-# stache_7.categories << cat_2
-# stache_8.categories << cat_2
-# stache_9.categories << cat_3
-# stache_10.categories << cat_3
-# stache_11.categories << cat_3
-# stache_12.categories << cat_3
-# stache_13.categories << cat_4
-# stache_14.categories << cat_4
-# stache_15.categories << cat_4
-# stache_16.categories << cat_4
+                     image_url: "http://i.imgur.com/XtsVPx6.png",
+                     stache_url: "http://i.imgur.com/UfIug97.png")
 
 user_1 = User.create(username: "beth", password: "123")
 stache_17 = Stache.create(name: "Mustachio",
