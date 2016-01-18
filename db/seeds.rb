38ade2c8413bc4d3e9471901dd783ac24f6c505c
Steven Olson <steveoscaro@gmail.com>
2016-01-18 00:03:25 -0700

    restyled the top navigation area - the links were pushing the logo down, which pushed the staches down, etc., so I split the links left and right along the top of the page, and moved the cart and logged in credentials down and to opposite sides of the page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,38 +5,42 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+description = "Raw denim paleo brunch, +1 cold-pressed fanny pack
+intelligentsia cred. Thundercats small batch gastropub, sartorial hella you
+probably haven't heard of them occupy before they sold out migas bicycle
+rights tacos"
 pic = "http://i.imgur.com/kMx8J0g.png"
-stache_1 = Stache.create(name: "Handlebar", description: "things", price: 5.0,
+stache_1 = Stache.create(name: "Handlebar", description: description, price: 5.0,
                          image_url: "http://i.imgur.com/4NHyiF6.png")
-stache_2 = Stache.create(name: "Chevron", description: "things", price: 3.0,
+stache_2 = Stache.create(name: "Chevron", description: description, price: 3.0,
                          image_url: "http://i.imgur.com/16uh0nF.png")
-stache_3 = Stache.create(name: "Dali", description: "things", price: 10.0,
+stache_3 = Stache.create(name: "Dali", description: description, price: 10.0,
                          image_url: "http://i.imgur.com/3J3agKC.png")
-stache_4 = Stache.create(name: "English", description: "things", price: 9.0,
+stache_4 = Stache.create(name: "English", description: description, price: 9.0,
                          image_url: "http://i.imgur.com/Ia027l3.png")
-stache_5 = Stache.create(name: "Fu Manchu", description: "things", price: 4.0,
+stache_5 = Stache.create(name: "Denim Soul", description: description, price: 4.0,
                          image_url: "http://i.imgur.com/29KrsNC.png")
-stache_6 = Stache.create(name: "Hippy", description: "things", price: 4.0,
+stache_6 = Stache.create(name: "Gastropub", description: description, price: 4.0,
                          image_url: "http://i.imgur.com/Rdrd1Ph.png")
-stache_7 = Stache.create(name: "Too Cool", description: "things", price: 4.0,
+stache_7 = Stache.create(name: "Too Cool", description: description, price: 4.0,
                          image_url: "http://i.imgur.com/sWH2YXv.png")
-stache_8 = Stache.create(name: "I Don't Care", description: "things", price: 4.0,
+stache_8 = Stache.create(name: "I Don't Care", description: description, price: 4.0,
                          image_url: "http://i.imgur.com/UaWJ4ub.png")
-stache_9 = Stache.create(name: "Arrrr", description: "things", price: 4.0,
+stache_9 = Stache.create(name: "Arrrr", description: description, price: 4.0,
                          image_url: "http://i.imgur.com/vctVE2S.png")
-stache_10 = Stache.create(name: "Matey", description: "things", price: 4.0,
+stache_10 = Stache.create(name: "Matey", description: description, price: 4.0,
                          image_url: "http://i.imgur.com/sqpx1Ah.png")
-stache_11 = Stache.create(name: "'Ol Wood Leg", description: "things", price: 4.0,
+stache_11 = Stache.create(name: "'Ol Wood Leg", description: description, price: 4.0,
                          image_url: "http://i.imgur.com/9bD5Rvo.png")
-stache_12 = Stache.create(name: "Shiver Me Timbers", description: "things", price: 4.0,
+stache_12 = Stache.create(name: "Shiver Me Timbers", description: description, price: 4.0,
                          image_url: "http://i.imgur.com/RShmizO.png")
-stache_13 = Stache.create(name: "Fabulous", description: "things", price: 4.0,
+stache_13 = Stache.create(name: "Fabulous", description: description, price: 4.0,
                          image_url: "http://i.imgur.com/oRpl6IL.png")
-stache_14 = Stache.create(name: "Diva", description: "things", price: 4.0,
+stache_14 = Stache.create(name: "Diva", description: description, price: 4.0,
                          image_url: "http://i.imgur.com/eV1i2oy.png")
-stache_15 = Stache.create(name: "Yay!", description: "things", price: 4.0,
+stache_15 = Stache.create(name: "Yay!", description: description, price: 4.0,
                          image_url: "http://i.imgur.com/LM4paeF.png")
-stache_16 = Stache.create(name: "Special", description: "things", price: 4.0,
+stache_16 = Stache.create(name: "Special", description: description, price: 4.0,
                          image_url: "http://i.imgur.com/XtsVPx6.png")
 
 cat_1 = Category.create(title: "Old Fashioned")
