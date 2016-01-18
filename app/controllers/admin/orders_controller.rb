679cd980a59ffac4ae4d6d0f6a8fa0a3985c54ef
Beth Secor <elizabeth.a.secor@gmail.com>
2016-01-17 22:30:32 -0700

    Implement feature for admin to change status to paid, cancelled, or completed

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,4 +3,14 @@ class Admin::OrdersController < ApplicationController
     Order.update(params[:order_id], status: "paid")
     redirect_to admin_dashboard_path
   end
+
+  def complete
+    Order.update(params[:order_id], status: "completed")
+    redirect_to admin_dashboard_path
+  end
+
+  def cancel
+    Order.update(params[:order_id], status: "cancelled")
+    redirect_to admin_dashboard_path
+  end
 end
--- test/integration/admin_view_orders_test.rb
+++ test/integration/admin_view_orders_test.rb
@@ -48,12 +48,63 @@ class AdminViewOrdersTest < ActionDispatch::IntegrationTest
 
     visit admin_dashboard_path
     click_on "order-#{order.id}-paid"
+    assert Order.find(order.id).status == "paid"
 
     assert_equal current_path, admin_dashboard_path
-    save_and_open_page
     within("#order-#{order.id}") do
       assert page.has_content?("Paid")
       refute page.has_content?("Ordered")
+    end
+  end
+
+  test "admin can change status of paid order to complete" do
+    create_eight_orders_for_user
+    admin = User.create(username: "admin", password: "pw", role: 1)
+    order = Order.find_by(status: "paid")
+    ApplicationController.any_instance.stubs(:current_user).returns(admin)
+
+    visit admin_dashboard_path
+    click_on "order-#{order.id}-complete"
+    assert Order.find(order.id).status == "completed"
+
+    assert_equal current_path, admin_dashboard_path
+    within("#order-#{order.id}") do
+      assert page.has_content?("Completed")
+      refute page.has_content?("Paid")
+    end
+  end
+
+  test "admin can change status of ordered order to cancelled" do
+    create_eight_orders_for_user
+    admin = User.create(username: "admin", password: "pw", role: 1)
+    order = Order.find_by(status: "ordered")
+    ApplicationController.any_instance.stubs(:current_user).returns(admin)
+
+    visit admin_dashboard_path
+    click_on "order-#{order.id}-cancel"
+    assert Order.find(order.id).status == "cancelled"
+
+    assert_equal current_path, admin_dashboard_path
+    within("#order-#{order.id}") do
+      assert page.has_content?("Cancelled")
+      refute page.has_content?("Ordered")
+    end
+  end
+
+  test "admin can change status of paid order to cancelled" do
+    create_eight_orders_for_user
+    admin = User.create(username: "admin", password: "pw", role: 1)
+    order = Order.find_by(status: "paid")
+    ApplicationController.any_instance.stubs(:current_user).returns(admin)
+
+    visit admin_dashboard_path
+    click_on "order-#{order.id}-cancel"
+    assert Order.find(order.id).status == "cancelled"
+
+    assert_equal current_path, admin_dashboard_path
+    within("#order-#{order.id}") do
+      assert page.has_content?("Cancelled")
+      refute page.has_content?("Paid")
     end
   end
 end
